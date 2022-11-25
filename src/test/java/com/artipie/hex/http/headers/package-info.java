9cc2e269fe94a6aae0162ddd6b35eec1b6665121
Aleksei Borodin <swizbizy@gmail.com>
2022-11-25 17:14:42 +0300

    feat: add support ContentType header for HexPm
    closes: #31

---
+++ src/test/java/com/artipie/hex/http/headers/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2022 artipie.com
+ * https://github.com/artipie/hexpm-adapter/blob/master/LICENSE.txt
+ */
+
+/**
+ * Tests for Hex headers.
+ *
+ * @since 0.2
+ */
+package com.artipie.hex.http.headers;
+
